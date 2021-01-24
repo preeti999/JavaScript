ac8572c5cbc91e08404023991e315d1338b8395f
preeti999 <preetik.ce1998@gmail.com>
2021-01-24 16:45:14 -0700

    eventlistener Project

---
+++ 4. GetComputedProject/script.js
@@ -0,0 +1,41 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor = (selectedElement) => {
+    return window.getComputedStyle(selectedElement).backgroundColor;
+}
+
+/*
+//grabing the color
+var color = getBGColor(pink);
+
+pink.addEventListener("click", () => {
+    center.style.background = color;
+});
+*/
+
+const magicColorChanger = (element, color) => {
+    return element.addEventListener("mouseover", () => {
+        center.style.background = color;
+    });
+
+}
+
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(pink, getBGColor(pink));
+
+
+// setInterval(() => {
+//     center.style.background="";
+// },2 )
