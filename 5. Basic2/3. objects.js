ae91794b8622cc17d68b67e11b530131ba64f741
preeti999 <preetik.ce1998@gmail.com>
2021-01-26 17:06:49 -0700

    objects in different way

---
+++ 5. Basic2/3. objects.js
@@ -0,0 +1,21 @@
+// Declearing a object through various manner
+
+var User = {
+    name: "",
+    getUserNAme: function () {
+        console.log(`USer name is : ${this.name}`);
+    },
+}
+
+var preeti = Object.create(User);
+console.log(preeti);
+
+preeti.name = "Preeti kumari";  // gives you the full user name
+preeti.getUserNAme();
+
+// caling a prototype without constuctor
+var shivam = Object.create(User, {
+    name: { value: "Shivam kumar" },
+    courseCount:{value:5}
+});
+shivam.getUserNAme();
