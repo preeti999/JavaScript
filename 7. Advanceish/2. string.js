646bcd0dc18bd25fec157ce6ae651a86e8fcfa94
preeti999 <preetik.ce1998@gmail.com>
2021-01-28 22:11:44 -0700

    string

---
+++ 7. Advanceish/2. string.js
@@ -0,0 +1,11 @@
+var firstNam = 'preeti';
+var lastName = "kumari";
+var cource = `${firstNam}'s first cource is computer basics`;
+
+console.log(cource);
+console.log(firstNam.charAt(0));
+console.log(firstNam.endsWith("y"));
+console.log(firstNam.includes("preeti"));
+console.log(firstNam.toLocaleUpperCase());
+
+
