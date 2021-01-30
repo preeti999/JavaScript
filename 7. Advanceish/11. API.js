a772865275028edc8da7b0b722889355bee3c0b9
preeti999 <preetik.ce1998@gmail.com>
2021-01-30 00:09:20 -0700

    API

---
+++ 7. Advanceish/11. API.js
@@ -0,0 +1,16 @@
+// Finding a joke from chucknorris
+
+// https://api.chucknorris.io/jokes/random
+// this code only working in chrome=>console
+
+fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => {
+        return response.json();
+    })
+
+    .then((data) => {
+        var joke = data.value;
+        console.log("JOKE: ", joke);
+    })
+    .catch();
+
